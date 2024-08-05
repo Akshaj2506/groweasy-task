178c1f12e7707013515a36aabb47b0346170007d
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2024-08-05 10:42:38 +0530

    updated banner component

--- app/components/BannerContainer.tsx
+++ app/components/BannerContainer.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+"use client"
+import React, { useEffect } from "react";
 import Banner from "./Banner";
 
-const BannerContainer = async () => {
-
-  const response = await fetch("http://localhost:8000/banners");
-  const banners: Banner[] = await response.json();
+const BannerContainer = () => {
+  const [banners, setBanners] = React.useState<Banner[] | null>(null);
+  async function fetchBanners(): Promise<Banner[] | null> {
+    try {
+      const res = await fetch("/banners.json");
+      if (!res.ok) {
+        throw new Error(`Error Status: ${res.status}`);
+      }
+      const data = await res.json();
+      console.log(data);
+      return data.banners;
+    } catch (error) {
+      console.error("Unable to fetch data:", error);
+      return null;
+    }
+  }
+  useEffect(() => {
+    const getBanners = async () => {
+      const data = await fetchBanners();
+      setBanners(data)
+    }
+    getBanners();
+  }, [])
 
   return (
     <div className="banner-container container grid grid-cols-3 gap-4 h-1/4 mt-2">
-      {banners.map((banner : Banner) => (
+      {banners?.map((banner : Banner) => (
         <Banner
           key={banner.id}
           id={banner.id}
